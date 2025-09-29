91d31f4dfda44f273b890e44f3cc22b289710cb3
Rashedul Alam <rashed464@outlook.com>
2025-09-29 12:18:42 +0600

    code updated

--- Library.Application/DTOs/BookDto.cs
+++ Library.Application/DTOs/BookDto.cs
@@ -8,27 +8,27 @@ namespace Library.Application.DTOs
     {
         public int Id { get; set; }
 
-       [Required]
-    [StringLength(200)]
-    public string Title { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
 
-    [StringLength(1000)]
-    public string Description { get; set; }
+        [StringLength(1000)]
+        public string Description { get; set; }
 
-    public int? AuthorId { get; set; }
-    public int? CategoryId { get; set; }
-    public int? PublisherId { get; set; }
+        public int? AuthorId { get; set; }
+        public int? CategoryId { get; set; }
+        public int? PublisherId { get; set; }
 
-    [DataType(DataType.Date)]
-    public DateTime? PublishDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? PublishDate { get; set; }
 
-    [Required]
-    [StringLength(13, MinimumLength = 10)]
-    public string ISBN { get; set; }
+        [Required]
+        [StringLength(13, MinimumLength = 10)]
+        public string ISBN { get; set; }
 
-    [Range(0, 9999.99)]
-    public decimal? Price { get; set; }
+        [Range(0, 9999.99)]
+        public decimal? Price { get; set; }
 
-    public bool? Active { get; set; }
+        public bool? Active { get; set; }
     }
 }
--- obj/Debug/net9.0/LibraryApiSqlServer.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryApiSqlServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApiSqlServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6345b10a5ef3fc3c35e75c8d453506bae89695c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b05f787e2027d3cd01a1fd762479f67b1981ef5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApiSqlServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApiSqlServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
