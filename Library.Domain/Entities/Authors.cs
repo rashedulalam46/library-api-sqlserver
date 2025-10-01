5b8ab0e6b132e620802c04ab2dad7aac11c4784e
Rashedul Alam <rashed464@outlook.com>
2025-10-01 22:50:41 +0600

    code updated

--- Library.Domain/Entities/Authors.cs
+++ Library.Domain/Entities/Authors.cs
@@ -12,23 +12,23 @@ public class Authors
 
 
     [Required(ErrorMessage = "Author name is required.")]
-    [StringLength(100)]
+    [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters.")]
     public string author_name { get; set; }
 
     [Required(ErrorMessage = "Country is required.")]
-    [StringLength(2)]
+    [StringLength(2, ErrorMessage = "Country code must be 2 characters.")]
     public string country { get; set; }
 
     [Required(ErrorMessage = "Address is required.")]
-    [StringLength(250)]
+    [StringLength(250, ErrorMessage = "Address cannot exceed 250 characters.")]
     public string address { get; set; }
 
     [Required(ErrorMessage = "Phone number is required.")]
-    [StringLength(20)]
+    [StringLength(20, ErrorMessage = "Phone number cannot exceed 20 characters.")]
     public string phone { get; set; }
 
     [StringLength(200)]
-    [EmailAddress]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
     public string email { get; set; }
 
     [DataType(DataType.Date, ErrorMessage = "Invalid date format.")]
