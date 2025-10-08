ee96989f425b7117d18bf5cd5a43d52b3df2f48f
Rashedul Alam <rashed464@outlook.com>
2025-10-08 22:23:40 +0600

    unit test proeject creatred

---
+++ LibraryApi.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryApi.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
--- obj/Debug/net9.0/LibraryApiSqlServer.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryApiSqlServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApiSqlServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c88bddb8e451957b7aef5922016216cd6bd980")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+348634986f6c911c90b1e9b19adcbe7d6a260650")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApiSqlServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApiSqlServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
